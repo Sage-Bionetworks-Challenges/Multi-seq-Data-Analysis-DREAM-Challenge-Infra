e3841a0827b98dc32d0c3cd0157d4b253ee01aa7
Rongrong Chai <rchai@sagebase.org>
2022-06-16 19:28:27 -0400

    increase docker mem to 80g

--- run_docker.py
+++ run_docker.py
@@ -113,7 +113,7 @@ def main(syn, args):
 
     # Assign different memory limit for different questions
     if args.question == "1":
-        docker_mem = "50g"
+        docker_mem = "80g"
     else:
         docker_mem = "6g"
 
