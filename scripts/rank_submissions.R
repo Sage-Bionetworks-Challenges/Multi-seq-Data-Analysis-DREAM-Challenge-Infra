a636397f22ab91a29befea338e564b30a67721d4
rrchai <73901500+rrchai@users.noreply.github.com>
2022-11-11 00:44:08 -0800

    update rank script for scatac

--- scripts/rank_submissions.R
+++ scripts/rank_submissions.R
@@ -26,8 +26,8 @@ for (task_n in seq_along(submission_views)) {
   # filter to subset of data used for public phase
   if (phase == "public" && task_n == 1) basenames <- basenames[grep("ds1_c4", basenames)]
   if (phase == "public" && task_n == 2) {
-    odd_pgs <- paste0("pg_", seq(1, by = 2, len = 16)) %>%
-      stringr::str_c(collapse = "|")
+    odd_pgs <- paste0("ds1.*pg_", seq(1, by = 2, len = 16)) %>%
+      stringr::str_c(collapse = "\\.|")
     basenames <- basenames[grep(odd_pgs, basenames)]
   }
   pred_filenames <- paste0(basenames, "_imputed.csv")
