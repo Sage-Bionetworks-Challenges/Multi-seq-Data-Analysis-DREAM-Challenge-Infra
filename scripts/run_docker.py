8c2cc445722fc00effd39f19267e59669966df3f
rrchai <73901500+rrchai@users.noreply.github.com>
2023-01-26 22:49:20 -0500

    test with 1g limit

--- scripts/run_docker.py
+++ scripts/run_docker.py
@@ -135,7 +135,7 @@ def main(syn, args):
 
     # Assign different resources limit for different questions
     # allow three submissions at a time
-    docker_mem = 160 if args.question == "1" else 5  # unit is Gib
+    docker_mem = 160 if args.question == "1" else 1  # unit is Gib
     docker_cpu = 20000000000 if args.question == "1" else 10000000000
     docker_runtime_quot = 43200 if args.public_phase else 86400
     pred_file_suffix = "*_imputed.csv" if args.question == "1" else "*.bed"
