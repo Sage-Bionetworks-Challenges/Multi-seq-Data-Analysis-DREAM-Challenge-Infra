5957e6e381b51316466a1587b92ac30e45650098
rrchai <73901500+rrchai@users.noreply.github.com>
2023-02-09 09:57:10 -0500

    update to reset values

--- annotate_latest_writeup.R
+++ annotate_latest_writeup.R
@@ -2,6 +2,18 @@ library(reticulate)
 library(dplyr)
 library(data.table)
 
+# put at the start of the line - sensitive to indents
+# utils <- py_run_string(
+# '
+# def reset_column(syn, table_id, col_name, col_type="STRING"):
+#     schema = syn.get(table_id)
+#     [schema.removeColumn(col) for col in syn.getColumns(schema) if col.name == col_name]
+#     new_column = syn.createColumn(name=col_name, columnType=col_type)
+#     schema.addColumn(new_column)
+#     syn.store(schema)
+#     '
+# )
+
 # set up env
 challengeutils <- import("challengeutils")
 syn <- import("synapseclient")$Synapse()
@@ -16,24 +28,30 @@ query <- sprintf("SELECT * FROM %s WHERE status = 'ACCEPTED'", writeup_id)
   
 sub_df <- syn$tableQuery(query)$asDataFrame() %>%
   select(id, submitterid, createdOn)
-  
+
 if (nrow(sub_df) == 0) {
     message("No valid submission found \u274C")
   } else {
   # get latest submission by submitter
-  writeup_df <-
+  annot_ids <-
     sub_df %>%
     group_by(submitterid) %>%
-    slice_max(createdOn,n = 1)
+    slice_max(createdOn,n = 1) %>% 
+    pull(id)
+  
+  # removing the column only remove it from schema,
+  # but doesn't reset the values of the column
+  # so have to re-annotate all rows for now
+  sub_df$isLatest <- ifelse(sub_df$id %in% annot_ids, TRUE, FALSE)
   
   tryCatch(
     {
-      for (j in 1:nrow(writeup_df)) { # use for loop to prevent from request error
-        # annotate each submission with its ranks
-        annots <- list(
-          isLatest = as.character("yes")
+      for (j in 1:nrow(sub_df)) { # use for loop to prevent from request error
+        challengeutils$annotations$annotate_submission(
+          syn, 
+          sub_df$id[j], 
+          list(isLatest = as.logical(sub_df$isLatest[j]))
         )
-        challengeutils$annotations$annotate_submission(syn, writeup_df$id[j], annots)
       }
       message("Annotating writeup submissions with 'isLatest' DONE \u2705")
     },
